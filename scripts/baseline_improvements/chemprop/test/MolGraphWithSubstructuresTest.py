db4851d1831b2e498d25cf311517413cb581930f
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2020-08-12 12:31:37 +0300

    MPAPC-56 More tests added

--- scripts/baseline_improvements/chemprop/test/MolGraphWithSubstructuresTest.py
+++ scripts/baseline_improvements/chemprop/test/MolGraphWithSubstructuresTest.py
@@ -9,7 +9,7 @@ class MolGraphWithSubstructuresTest(unittest.TestCase):
     def test_bonds_count(self):
         args = TrainArgs()
         args.substructures_merge = False
-        args.substructures_use_substructures = False
+        args.substructures_use_substructures = True
 
         smi = "Nc1ccc(O)c2ncccc12"
         mol = MolGraphWithSubstructures(smi, args)
@@ -23,6 +23,24 @@ class MolGraphWithSubstructuresTest(unittest.TestCase):
         smi = "C1=CC=C(C(=C1CC(C)C)CCCCNCCN)C(=O)O"
         mol = MolGraphWithSubstructures(smi, args)
         self.assertEqual(len(mol.f_bonds), 26)
+
+    def test_atoms_count(self):
+        args = TrainArgs()
+        args.substructures_merge = False
+        args.substructures_use_substructures = True
+
+        smi = "Nc1ccc(O)c2ncccc12"
+        mol = MolGraphWithSubstructures(smi, args)
+        self.assertEqual(len(mol.f_atoms), 4)
+
+    def test_atoms_count_for_big_molecule(self):
+        args = TrainArgs()
+        args.substructures_merge = False
+        args.substructures_use_substructures = True
+
+        smi = "C1=CC=C(C(=C1CC(C)C)CCCCNCCN)C(=O)O"
+        mol = MolGraphWithSubstructures(smi, args)
+        self.assertEqual(len(mol.f_atoms), 14)
 
 
 if __name__ == '__main__':
