ad45ef0d22848e825e245e6b09a75fe1b533b1b7
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2020-08-14 14:46:52 +0300

    MPAPC-57 Summaries updated, code refactoring

--- scripts/preprocess/merge_train_validate.py
+++ scripts/preprocess/merge_train_validate.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-train = pd.read_csv("logp_wo_averaging_train.csv")
-valid = pd.read_csv("logp_wo_averaging_validation.csv")
+DATA_PATH = "../../data/3_final_data/split_data/"
+
+train = pd.read_csv(DATA_PATH + "logp_wo_averaging_train.csv")
+valid = pd.read_csv(DATA_PATH + "logp_wo_averaging_validation.csv")
 
 sum_data = pd.concat([train, valid])
-sum_data.to_csv("logp_wo_averaging_cross.csv", index=False)
+sum_data.to_csv(DATA_PATH + "logp_wo_averaging_cross.csv", index=False)
