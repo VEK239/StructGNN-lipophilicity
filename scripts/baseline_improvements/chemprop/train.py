1a14e992fd43ac872e79955ec23a1ff731c51671
VEK239 <vlasova.elizaveta@yandex.ru>
2020-08-19 14:24:22 +0000

    MPAPC-66 Added trainig pipeline for best model with substructures 2/1/500/500

--- scripts/baseline_improvements/chemprop/train.py
+++ scripts/baseline_improvements/chemprop/train.py
@@ -1,4 +1,6 @@
 """Trains a chemprop model on a dataset."""
+import sys
+sys.path.append('/home/mol/liza/mol_properties')
 
 from scripts.baseline_improvements.chemprop.train import chemprop_train
 
