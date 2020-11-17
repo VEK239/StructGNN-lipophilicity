e4a0a9810e13f5a122c088d14b3311ba09164367
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2020-11-17 12:04:06 +0300

    MPAPC-89 Added train-val merged dataset

---
+++ merge_train_val.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+train = pd.read_csv('./data/3_final_data/split_data/logp_wo_logp_json_wo_averaging_train.csv')
+val = pd.read_csv('./data/3_final_data/split_data/logp_wo_logp_json_wo_averaging_validation.csv')
+train_val = pd.concat([train, val], ignore_index=True)
+train_val = train_val.drop(columns=['Unnamed: 0'])
+print(train_val)
+train_val.to_csv('./data/3_final_data/split_data/logp_wo_logp_json_wo_averaging_train_val.csv', index=False)
