6e103091a4f2843f8dcb47a78e270ad2ff5e624e
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2020-11-17 10:43:47 +0300

    MPAPC-89 Moved from chainer to torch

--- scripts/baseline_improvements/chemprop/models/weavenet.py
+++ scripts/baseline_improvements/chemprop/models/weavenet.py
@@ -1,6 +1,6 @@
-import chainer
-from chainer import functions
-from chainer import links
+import torch
+from torch import nn
+from torch.nn import functional
 from .embed_atom_id import EmbedAtomID
 
 MAX_ATOMIC_NUM = 170
@@ -25,107 +25,98 @@ from features import BatchMolGraph, get_atom_fdim, get_bond_fdim, mol2graph, \
 WEAVENET_DEFAULT_WEAVE_CHANNELS = [50, ]
 
 
-class LinearLayer(chainer.Chain):
+class LinearLayer(nn.Module):
 
     def __init__(self, n_channel, n_layer):
         super(LinearLayer, self).__init__()
-        with self.init_scope():
-            self.layers = chainer.ChainList(
-                *[links.Linear(None, n_channel) for _ in range(n_layer)]
-            )
+
+        self.layers = [nn.Linear(None, n_channel) for _ in range(n_layer)]
         self.n_output_channel = n_channel
 
     def forward(self, x):
         n_batch, n_atom, n_channel = x.shape
-        x = functions.reshape(x, (n_batch * n_atom, n_channel))
+        x = functional.reshape(x, (n_batch * n_atom, n_channel))
         for l in self.layers:
             x = l(x)
-            x = functions.relu(x)
-        x = functions.reshape(x, (n_batch, n_atom, self.n_output_channel))
+            x = functional.relu(x)
+        x = functional.reshape(x, (n_batch, n_atom, self.n_output_channel))
         return x
 
 
-class AtomToPair(chainer.Chain):
+class AtomToPair(nn.Module):
     def __init__(self, n_channel, n_layer, n_atom):
         super(AtomToPair, self).__init__()
-        with self.init_scope():
-            self.linear_layers = chainer.ChainList(
-                *[links.Linear(None, n_channel) for _ in range(n_layer)]
-            )
+        self.linear_layers = [nn.Linear(None, n_channel) for _ in range(n_layer)]
         self.n_atom = n_atom
         self.n_channel = n_channel
 
     def forward(self, x):
         n_batch, n_atom, n_feature = x.shape
-        atom_repeat = functions.reshape(x, (n_batch, 1, n_atom, n_feature))
-        atom_repeat = functions.broadcast_to(
+        atom_repeat = functional.reshape(x, (n_batch, 1, n_atom, n_feature))
+        atom_repeat = functional.broadcast_to(
             atom_repeat, (n_batch, n_atom, n_atom, n_feature))
-        atom_repeat = functions.reshape(atom_repeat,
+        atom_repeat = functional.reshape(atom_repeat,
                                         (n_batch, n_atom * n_atom, n_feature))
 
-        atom_tile = functions.reshape(x, (n_batch, n_atom, 1, n_feature))
-        atom_tile = functions.broadcast_to(
+        atom_tile = functional.reshape(x, (n_batch, n_atom, 1, n_feature))
+        atom_tile = functional.broadcast_to(
             atom_tile, (n_batch, n_atom, n_atom, n_feature))
-        atom_tile = functions.reshape(atom_tile,
+        atom_tile = functional.reshape(atom_tile,
                                       (n_batch, n_atom * n_atom, n_feature))
 
-        pair_x0 = functions.concat((atom_tile, atom_repeat), axis=2)
-        pair_x0 = functions.reshape(pair_x0,
+        pair_x0 = torch.cat((atom_tile, atom_repeat), axis=2)
+        pair_x0 = functional.reshape(pair_x0,
                                     (n_batch * n_atom * n_atom, n_feature * 2))
         for l in self.linear_layers:
             pair_x0 = l(pair_x0)
-            pair_x0 = functions.relu(pair_x0)
-        pair_x0 = functions.reshape(pair_x0,
+            pair_x0 = functional.relu(pair_x0)
+        pair_x0 = functional.reshape(pair_x0,
                                     (n_batch, n_atom * n_atom, self.n_channel))
 
-        pair_x1 = functions.concat((atom_repeat, atom_tile), axis=2)
-        pair_x1 = functions.reshape(pair_x1,
+        pair_x1 = torch.cat((atom_repeat, atom_tile), axis=2)
+        pair_x1 = functional.reshape(pair_x1,
                                     (n_batch * n_atom * n_atom, n_feature * 2))
         for l in self.linear_layers:
             pair_x1 = l(pair_x1)
-            pair_x1 = functions.relu(pair_x1)
-        pair_x1 = functions.reshape(pair_x1,
+            pair_x1 = functional.relu(pair_x1)
+        pair_x1 = functional.reshape(pair_x1,
                                     (n_batch, n_atom * n_atom, self.n_channel))
         return pair_x0 + pair_x1
 
 
-class PairToAtom(chainer.Chain):
+class PairToAtom(nn.Module):
     def __init__(self, n_channel, n_layer, n_atom, mode='sum'):
         super(PairToAtom, self).__init__()
-        with self.init_scope():
-            self.linearLayer = chainer.ChainList(
-                *[links.Linear(None, n_channel) for _ in range(n_layer)]
-            )
+        self.linearLayer = [nn.Linear(None, n_channel) for _ in range(n_layer)]
         self.n_atom = n_atom
         self.n_channel = n_channel
         self.mode = mode
 
     def forward(self, x):
         n_batch, n_pair, n_feature = x.shape
-        a = functions.reshape(
+        a = functional.reshape(
             x, (n_batch * (self.n_atom * self.n_atom), n_feature))
         for l in self.linearLayer:
             a = l(a)
-            a = functions.relu(a)
-        a = functions.reshape(a, (n_batch, self.n_atom, self.n_atom,
+            a = functional.relu(a)
+        a = functional.reshape(a, (n_batch, self.n_atom, self.n_atom,
                                   self.n_channel))
-        a = functions.sum(a, axis=2)
+        a = functional.sum(a, axis=2)
         return a
 
 
-class WeaveModule(chainer.Chain):
+class WeaveModule(nn.Module):
 
     def __init__(self, n_atom, output_channel, n_sub_layer,
                  readout_mode='sum'):
         super(WeaveModule, self).__init__()
-        with self.init_scope():
-            self.atom_layer = LinearLayer(output_channel, n_sub_layer)
-            self.pair_layer = LinearLayer(output_channel, n_sub_layer)
-            self.atom_to_atom = LinearLayer(output_channel, n_sub_layer)
-            self.pair_to_pair = LinearLayer(output_channel, n_sub_layer)
-            self.atom_to_pair = AtomToPair(output_channel, n_sub_layer, n_atom)
-            self.pair_to_atom = PairToAtom(output_channel, n_sub_layer, n_atom,
-                                           mode=readout_mode)
+        self.atom_layer = LinearLayer(output_channel, n_sub_layer)
+        self.pair_layer = LinearLayer(output_channel, n_sub_layer)
+        self.atom_to_atom = LinearLayer(output_channel, n_sub_layer)
+        self.pair_to_pair = LinearLayer(output_channel, n_sub_layer)
+        self.atom_to_pair = AtomToPair(output_channel, n_sub_layer, n_atom)
+        self.pair_to_atom = PairToAtom(output_channel, n_sub_layer, n_atom,
+                                       mode=readout_mode)
         self.n_atom = n_atom
         self.n_channel = output_channel
         self.readout_mode = readout_mode
@@ -133,21 +124,21 @@ class WeaveModule(chainer.Chain):
     def forward(self, atom_x, pair_x, atom_only=False):
         a0 = self.atom_to_atom.forward(atom_x)
         a1 = self.pair_to_atom.forward(pair_x)
-        a = functions.concat([a0, a1], axis=2)
+        a = torch.cat([a0, a1], axis=2)
         next_atom = self.atom_layer.forward(a)
-        next_atom = functions.relu(next_atom)
+        next_atom = functional.relu(next_atom)
         if atom_only:
             return next_atom
 
         p0 = self.atom_to_pair.forward(atom_x)
         p1 = self.pair_to_pair.forward(pair_x)
-        p = functions.concat([p0, p1], axis=2)
+        p = torch.cat([p0, p1], axis=2)
         next_pair = self.pair_layer.forward(p)
-        next_pair = functions.relu(next_pair)
+        next_pair = functional.relu(next_pair)
         return next_atom, next_pair
 
 
-class WeaveNet(chainer.Chain):
+class WeaveNet(nn.Module):
     """WeaveNet implementation
 
     Args:
@@ -166,16 +157,16 @@ class WeaveNet(chainer.Chain):
                  n_sub_layer=1, n_atom_types=MAX_ATOMIC_NUM,
                  readout_mode='sum'):
         weave_channels = weave_channels or WEAVENET_DEFAULT_WEAVE_CHANNELS
-        weave_module = [
+        self.weave_module = [
             WeaveModule(n_atom, c, n_sub_layer, readout_mode=readout_mode)
             for c in weave_channels
         ]
 
         super(WeaveNet, self).__init__()
-        with self.init_scope():
-            self.embed = EmbedAtomID(out_size=hidden_dim, in_size=n_atom_types)
-            self.weave_module = chainer.ChainList(*weave_module)
-            # self.readout = GeneralReadout(mode=readout_mode)
+        # with self.init_scope():
+        #     # self.embed = EmbedAtomID(out_size=hidden_dim, in_size=n_atom_types)
+        #     self.weave_module = chainer.ChainList(*weave_module)
+        #     # self.readout = GeneralReadout(mode=readout_mode)
         self.readout_mode = readout_mode
 
     def __call__(self, atom_x, pair_x, batch):
@@ -186,9 +177,9 @@ class WeaveNet(chainer.Chain):
             mol = graph.mol
             atom_x = mol.get_atom_features_vector()
             pair_x = mol.construct_pair_feature(num_max_atoms=WEAVE_DEFAULT_NUM_MAX_ATOMS)
-            if atom_x.dtype == self.xp.int32:
-                # atom_array: (minibatch, atom)
-                atom_x = self.embed(atom_x)
+            # if atom_x.dtype == self.xp.int32:
+            #     # atom_array: (minibatch, atom)
+            #     atom_x = self.embed(atom_x)
 
             for i in range(len(self.weave_module)):
                 if i == len(self.weave_module) - 1:
