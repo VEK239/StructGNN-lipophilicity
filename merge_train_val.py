699272a55da4705e1fef5f9a049ca21eb73a033c
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2020-11-19 14:07:47 +0300

    MPAPC-89 Renamed train-val file

--- merge_train_val.py
+++ merge_train_val.py
@@ -4,5 +4,4 @@ train = pd.read_csv('./data/3_final_data/split_data/logp_wo_logp_json_wo_averagi
 val = pd.read_csv('./data/3_final_data/split_data/logp_wo_logp_json_wo_averaging_validation.csv')
 train_val = pd.concat([train, val], ignore_index=True)
 train_val = train_val.drop(columns=['Unnamed: 0'])
-print(train_val)
-train_val.to_csv('./data/3_final_data/split_data/logp_wo_logp_json_wo_averaging_train_val.csv', index=False)
+train_val.to_csv('./data/3_final_data/split_data/logp_wo_logp_json_wo_averaging_train_val_dataset.csv', index=False)
